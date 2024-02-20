c0cedee4443902c01b77e39214637bef8951506a
Sobanim <sobolev.dmirtiy@gmail.com>
2024-02-20 08:19:16 +0100

    Rebuild project

--- app/index.js
+++ app/index.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import {Text, View} from "react-native";
 
 const Home = () => {
     return (
         <View>
-            <Text>
-                Home
-            </Text>
+            <Text>Home</Text>
         </View>
-    )
-}
+    );
+};
+
+export default Home;
